b9b90c84216262a3bfdd9b0a65fff90727599ec7
franck403 <79372051+franck403@users.noreply.github.com>
2024-01-05 13:57:23 -0500

    Create editor.js

---
+++ editor.js
@@ -0,0 +1,36 @@
+// api
+window.onmessage = function (e) {
+    if (e.data.slice(0, 4) == 'file') {
+        window.actvalue = e.data.slice(5,e.data.length)
+        require.config({ paths: { vs: 'https://cdn.jsdelivr.net/npm/monaco-editor@0.39.0/min/vs' } });
+        require(['vs/editor/editor.main'], function () {
+            monaco.editor.setTheme(
+                'dark'
+            )
+            var editor = monaco.editor.create(document.getElementById('container'), {
+                value: window.actvalue,
+                language: 'javascript',
+                fontSize: "20px",
+                theme: 'vs-dark'
+            });
+            monaco.languages.registerCompletionItemProvider('javascript', {
+                provideCompletionItems: (model, position) => {
+                    const keywords = [["Novatike", "/* --- Novakite --- */"]]
+                    const suggestions = keywords.map(keyword => ({
+                        label: keyword[0],
+                        kind: monaco.languages.CompletionItemKind.Keyword,
+                        insertText: keyword[1]
+                    }));
+                    return { suggestions };
+                }
+            });
+            editor.onDidChangeModelContent(function (e) {
+                e = monaco.editor.getEditors()[0].getValue()
+                window.top.postMessage('u' + e, '*')
+            });
+        });
+
+    }
+};
+
+window.top.postMessage('file', '*')
