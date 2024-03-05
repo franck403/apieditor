a9f03e11c743d95b9cd3993f0fbdbe6e153c1e09
franck403 <79372051+franck403@users.noreply.github.com>
2024-03-05 07:47:30 -0500

    Update default.js

--- default.js
+++ default.js
@@ -1,23 +1,30 @@
 // api
 window.onmessage = function (e) {
     var content = e.data
+    console.log(content)
+    console.log(typeof(content))
+    if (typeof(content) != typeof("string")) {return}
     if (content.slice(0, 4) == 'file') {
         window.actvalue = content.slice(5,e.data.length)
         require.config({ paths: { vs: 'https://cdn.jsdelivr.net/npm/monaco-editor@0.39.0/min/vs' } });
         require(['vs/editor/editor.main'], function () {
+            var onend = monaco.editor.onDidCreateEditor(function (event) {
+        		console.log('apieditor is ready to use');
+                window.parent.postMessage('ready', '*')
+        	});
             monaco.editor.setTheme(
                 'dark'
             )
             var editor = monaco.editor.create(document.getElementById('container'), {
                 value: window.actvalue,
-                language: 'repviewer',
+                language: 'RepViewer',
                 fontSize: "20px",
                 automaticLayout: true,
                 theme: 'vs-dark'
             });
             editor.onDidChangeModelContent(function (e) {
                 e = monaco.editor.getEditors()[0].getValue()
-                window.top.postMessage('u' + e, '*')
+                window.parent.postMessage('u' + e, '*')
             });
         });
 
