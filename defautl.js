8658211c1b90ede427830ab7c68b06b9f62ffd67
franck403 <79372051+franck403@users.noreply.github.com>
2024-03-04 21:05:44 -0500

    Create defautl.js

---
+++ defautl.js
@@ -0,0 +1,25 @@
+// api
+window.onmessage = function (e) {
+    var content = e.data
+    if (content.slice(0, 4) == 'file') {
+        window.actvalue = content.slice(5,e.data.length)
+        require.config({ paths: { vs: 'https://cdn.jsdelivr.net/npm/monaco-editor@0.39.0/min/vs' } });
+        require(['vs/editor/editor.main'], function () {
+            monaco.editor.setTheme(
+                'dark'
+            )
+            var editor = monaco.editor.create(document.getElementById('container'), {
+                value: window.actvalue,
+                fontSize: "20px",
+                theme: 'vs-dark'
+            });
+            editor.onDidChangeModelContent(function (e) {
+                e = monaco.editor.getEditors()[0].getValue()
+                window.top.postMessage('u' + e, '*')
+            });
+        });
+
+    }
+};
+
+window.parent.postMessage('file', '*')
